412c67cc7550dd0653eb428b2e2bc06a05a02551
harpreet16 <harpreet.kaur@innoraft.com>
2017-04-10 15:17:55 +0530

    database check done

--- includes/databaseservices.php
+++ includes/databaseservices.php
@@ -3,13 +3,25 @@
 		class DatabaseServices {
 
 		     function insertuserinfo($output) {
+				
 		        $db =mysqli_connect("localhost","root","123","db_badge");        
 		        $data = json_decode($output, true);        
 		        $login=$data["login"];
 		        $email=$data["email"];
+		        $query1="SELECT * FROM t_users WHERE 'user_id'='$login' AND 'user_email'='$email'";
+
+				$res = $db->query($query1);
+
+				if ($res)
+
+				{
+				    echo "you are already registered";
+				}
+				else {  
 		        $sql= "INSERT INTO t_users(user_id,user_email) VALUES('$login','$email')";
 		        mysqli_query($db,$sql);
 		        echo "connected";
-		    }
+		    	}
+			}
 		 }
  ?>
