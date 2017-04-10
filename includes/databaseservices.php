fafd1d8efb6364b300d22719811262839be8aca2
harpreet16 <harpreet.kaur@innoraft.com>
2017-04-10 17:56:35 +0530

    duplicate id ruled out

--- includes/databaseservices.php
+++ includes/databaseservices.php
@@ -3,19 +3,23 @@
 		class DatabaseServices {
 
 		     function insertuserinfo($output) {
+		     	
 				
 		        $db =mysqli_connect("localhost","root","123","db_badge");        
 		        $data = json_decode($output, true);        
 		        $login=$data["login"];
 		        $email=$data["email"];
-		        $query1="SELECT * FROM t_users WHERE 'user_id'='$login' AND 'user_email'='$email'";
+
+		        $query1="SELECT * FROM t_users WHERE user_id='".$login."' AND user_email='".$email."'";
 
 				$res = $db->query($query1);
+				$num= $res->num_rows;
 
-				if ($res)
+				if ($num >=1)
 
 				{
 				    echo "you are already registered";
+				    
 				}
 				else {  
 		        $sql= "INSERT INTO t_users(user_id,user_email) VALUES('$login','$email')";
